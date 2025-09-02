6ee53ee29171b0cd5db194326d93b814ed782e4e
BIKRAM TRIPATHI <102977156+Funza07@users.noreply.github.com>
2025-09-02 15:32:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Black Hole Game JavaScript
+// Note: All JavaScript is included inline in the HTML file for better reliability
+// This file is kept minimal as the main logic is in the HTML file
+
+console.log('Black Hole Game app.js loaded');
+
